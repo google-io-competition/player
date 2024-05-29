69142f38ea9d58f61f2045dd8eb9d9a3741340c1
alexw <alex.j.witkowski@gmail.com>
2024-05-29 00:10:45 -0400

    Created main menu screen

---
+++ player/lib/screen/menu.dart
@@ -0,0 +1,75 @@
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:player/style/themed_button.dart';
+import 'package:provider/provider.dart';
+
+import '../settings/settings.dart';
+import '../style/audio/audio_controller.dart';
+import '../style/audio/sounds.dart';
+import '../style/pallette.dart';
+import '../style/themed_screen.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final palette = context.watch<Palette>();
+    final settingsController = context.watch<SettingsController>();
+    final audioController = context.watch<AudioController>();
+
+    return Scaffold(
+      backgroundColor: palette.backgroundMain,
+      body: ResponsiveScreen(
+        squarishMainArea: Center(
+          child: Transform.rotate(
+            angle: -0.1,
+            child: const Text(
+              'Drag&Drop Cards!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Permanent Marker',
+                fontSize: 55,
+                height: 1,
+              ),
+            ),
+          ),
+        ),
+        rectangularMenuArea: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ThemedButton(
+              onPressed: () {
+                audioController.playSfx(SfxType.buttonTap);
+                GoRouter.of(context).go('/play');
+              },
+              child: const Text('Play'),
+            ),
+            _gap,
+            ThemedButton(
+              onPressed: () => GoRouter.of(context).push('/settings'),
+              child: const Text('Settings'),
+            ),
+            _gap,
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: ValueListenableBuilder<bool>(
+                valueListenable: settingsController.audioOn,
+                builder: (context, audioOn, child) {
+                  return IconButton(
+                    onPressed: () => settingsController.toggleAudioOn(),
+                    icon: Icon(audioOn ? Icons.volume_up : Icons.volume_off),
+                  );
+                },
+              ),
+            ),
+            _gap,
+          ],
+        ),
+      ),
+    );
+  }
+
+  static const _gap = SizedBox(height: 10);
+}
